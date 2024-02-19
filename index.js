4a608ec8a1652ad9a9a086ddacc48c2bc7e20193
Jeck <zrkeimperial@gmail.com>
2024-02-19 10:28:50 +0800

    cursor pointer update

--- index.js
+++ index.js
@@ -16,3 +16,47 @@ document.onkeydown = function(e) {
   if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) return false; 
   if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0))return false; 
 } 
+
+// Cursor Pointer
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
+
+const colors = [
+  "#4800ff", "#3b0bed", "#2e11dc", "#2214ca", "#1515b8", "#0816a7", "#001595", "#001484", "#001273", "#001062", "#000d52", "#000842"
+];
+
+circles.forEach(function (circle, index) {
+  circle.x = 0;
+  circle.y = 0;
+  //circle.style.backgroundColor = colors[index % colors.length];
+});
+
+window.addEventListener("mousemove", function(e){
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+  
+});
+
+function animateCircles() {
+  
+  let x = coords.x;
+  let y = coords.y;
+  
+  circles.forEach(function (circle, index) {
+    circle.style.left = x + "px";
+    circle.style.top = y + "px";
+    
+    circle.style.scale = (circles.length - index) / circles.length;
+    
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+  });
+ 
+  requestAnimationFrame(animateCircles);
+}
+
+animateCircles();
